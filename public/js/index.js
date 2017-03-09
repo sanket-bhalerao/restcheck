62a20ceac540f0e6636bd78b026f577764dcfc02
Sanket Bhalerao <sanket_bhalerao@persistent.co.in>
2017-03-09 21:08:17 +0530

    initial checkin

---
+++ public/js/index.js
@@ -0,0 +1,91 @@
+/**
+ * javascript
+ * */
+// var updateButton = document.getElementById("update");
+
+function updatefun() {
+    document.getElementById("wsresponse").innerHTML = "";
+    var form = document.getElementById("myForm");
+    // collect the form data while iterating over the inputs
+    var data = {};
+    for (var i = 0, ii = form.length; i < ii; ++i) {
+        var input = form[i];
+        if (input.name && input.type != "button") {
+            if (input.name == "measurementDate") {
+                if (input.value === "") {
+                    document.getElementById("wsresponse").innerHTML = "Date can not be blank.";
+                    return false;
+                } else {
+                    var dd = new Date(input.value);
+                    data[input.name] = dd.toISOString();
+                }
+            } else if (input.name == "referenceNumber") {
+                if (input.value === "") {
+                    document.getElementById("wsresponse").innerHTML = "Reference Number can not be blank";
+                    return false;
+                } else {
+                    data[input.name] = input.value;
+                }
+            } else
+                if (input.value === "") {
+                    data[input.name] = null;                    
+                } else {
+                    data[input.name] = Number(input.value);
+                }
+
+        }
+    }
+    //action="https://maw-simulatorui.mybluemix.net/submeter"
+    //  method="POST" 
+    console.log(JSON.stringify(data));
+    // construct an HTTP request
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+    };
+    xhr.open("POST", "/submeter", true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    // send the collected data as JSON
+    xhr.send(JSON.stringify(data));
+    wsFunction();
+}
+// var cancelButton = document.getElementById("reset");
+
+function resetfun() {
+    var form = document.getElementById("myForm");
+    for (var i = 0, ii = form.length; i < ii; ++i) {
+        var input = form[i];
+        if (input.name && input.type != "button") {
+            input.value = "";
+        }
+    }
+}
+
+function wsFunction() {
+    var ws = new WebSocket("wss://maw-simulatorui.mybluemix.net/maw/ws");
+    ws.onmessage = function (evt) {
+        var received_msg = evt.data;
+        var parsedMsg = JSON.parse(received_msg);
+        var innerMsg = parsedMsg.results[0];
+        var msgType = "";
+        var retVal = "";
+        for (var key in innerMsg) {
+            console.log("Key: " + key);
+            console.log("Value: " + innerMsg[key]);
+            msgType = key;
+            retVal = innerMsg[key];
+            break;
+        }
+        if (key === "id") {
+            document.getElementById("wsresponse").innerHTML = "Record processed successfully, returned id: " + retVal;
+        } else {
+            document.getElementById("wsresponse").innerHTML = "Record processing failed, error message: " + retVal.entity.errorMessages[0];
+        }
+
+        // document.getElementById("wsresponse1").innerHTML = received_msg;
+        resetfun();
+        ws.close();
+    };
+}
---
+++ server.js
@@ -0,0 +1,73 @@
+/*eslint-env node */
+/*eslint indent:0,semi:0 */
+/*jshint devel: true, node: true*/
+/*global: */
+
+/***
+ * Start an instance of Node-Red under Express.JS
+ *
+ * Allows multiple instances of Node-Red to be run, even different versions in parallel.
+ ***/
+
+'use strict';  /* always for Node.JS, never global in the browser */
+var cfenv = require('cfenv');
+var http 	= require('https'),
+    //https 	= require('https'),
+    express = require('express'),  // THE std library for serving HTTP
+    RED     = require('node-red'),
+    nrSettings = require('./settings.js') // Node-Red settings file
+;
+var appEnv = cfenv.getAppEnv();
+// The TCP port for this systems web interface - picked up from env, package.json or fixed value
+var nrPort = appEnv.port || process.env.LOCALPORT || process.env.npm_package_config_localPort || '1880';
+
+// Create an Express app
+var app = express();
+
+// Add a simple route for static content served from './public'
+app.use( '/', express.static('public') );
+
+// Add static route for bower components from './bower_components'
+app.use( '/bower_components', express.static('bower_components') );
+
+// Create the http server
+// TODO: Add code for https server
+var httpServer = http.createServer(app);
+
+// Initialise the runtime with a server and settings
+// @see http://nodered.org/docs/configuration.html
+RED.init( httpServer, nrSettings );
+
+// Serve the editor UI from /admin
+app.use( nrSettings.httpAdminRoot, RED.httpAdmin );
+
+// Serve the http nodes from /
+app.use( nrSettings.httpNodeRoot, RED.httpNode );
+
+httpServer.listen( nrPort, function(){
+  console.log('Express 4 server listening on port %s, serving node-red', nrPort);
+});
+var options = {
+  host: "pxc4ql.internetofthings.ibmcloud.com",
+  path: '/api/v0002/device/types/mATwDevType/devices/OWASA-WTUL9215MO',
+  method: 'GET',
+  auth:'a-pxc4ql-6w29om6quk:4p_(w3Z5e612@1t99R'
+};
+app.get("/devices",function(request,response){
+  var resJson = null;
+  var retJson = {};
+  http.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (data) {
+      resJson=data;
+    });
+  }).end();
+  for(var idx = 0; idx< data.results ; idx++){
+    if(data.results[idx].typeId == "mATwDevType"){
+      retJson[data.results[idx].deviceId]=data.results[idx].metadata;
+    }
+  }
+  response.json(retJson);
+});
+// Start the runtime
+RED.start();
